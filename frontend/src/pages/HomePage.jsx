81088cf7fdc90b8290f4f39a5eaffa7aee098328
venre1 <128736471+venre1@users.noreply.github.com>
2025-05-22 23:36:01 +0100

    home page commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react'
-import Navbar from '../components/navbar' 
+import Navbar from '../components/Navbar' 
 import RateLimitedUI from '../components/RateLimitedUI';
 import api from '../lib/axios';
 import toast from 'react-hot-toast';
